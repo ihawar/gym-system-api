ef6e14b10e3716b75ea114d29631b58007f98380
hawar <iaramkhzri@gmail.com>
2026-02-08 19:57:33 +0330

    parsePhone implemented

---
+++ src/utils/parsePhone.ts
@@ -0,0 +1,18 @@
+export default function parsePhone(txt: string) {
+  let phone = '98';
+  if (txt.startsWith('09') && txt.length == 11) {
+    phone += txt.substring(1);
+  } else if (txt.startsWith('+989') && txt.length == 13) {
+    phone += txt.substring(3);
+  } else if (txt.startsWith('989') && txt.length == 12) {
+    phone += txt.substring(2);
+  } else if (txt.startsWith('9') && txt.length == 10) {
+    phone += txt;
+  }
+
+  if (/^989\d{9}$/.test(phone)) {
+    return phone;
+  } else {
+    throw Error('Invalid phone number format.');
+  }
+}
