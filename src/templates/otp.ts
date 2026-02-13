a1e672ee5c3d34266c4d1d1c8810206927117b0e
hawar <iaramkhzri@gmail.com>
2026-02-13 04:45:03 +0330

    otp templated added

---
+++ src/templates/otp.ts
@@ -0,0 +1,17 @@
+export const CREATE_ACCOUNT_MESSAGE = `
+کد ورود شما:
+\n
+{code}
+\n
+*این کد را در اختیار هیچکس قرار ندهید.
+`;
+
+export const RESET_PASSWORD_MESSAGE = `
+کد تایید شما:
+\n
+{code}
+\n
+*این کد برای عوض کردن رمز عبور شما به کار میرود. در اختیار هیچ کس قرار ندهید.
+`;
+
+export const KAVENEGRAR_URL = `https://api.kavenegar.com/v1/{api_key}/sms/send.json?receptor={recipient}&sender={sender}&message={msg}`;
