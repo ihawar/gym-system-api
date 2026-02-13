593804aa0f74cfb1c6c4183ec1ebe5e37f7e9a73
hawar <iaramkhzri@gmail.com>
2026-02-13 04:42:57 +0330

    error middleware added

---
+++ src/middlewares/error.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import { AppError } from '@/types/customError';
+import logger from '@/lib/logger';
+
+// Handle not founds
+export function notFound(req: Request, res: Response, next: NextFunction) {
+  next(new AppError(`Not Found - ${req.originalUrl}`, 404, true));
+}
+
+export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+  // Decide status & message
+  let statusCode = 500;
+  let message = 'Something went wrong';
+
+  // Our custom errors
+  if (err instanceof AppError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  }
+
+  logger.error(err);
+
+  // Send response
+  res.status(statusCode).json({
+    success: false,
+    error: message,
+    // only show stack in development
+    ...(process.env.NODE_ENV !== 'production' && { stack: err.stack }),
+  });
+}
