a4415a0dd0a68983778a869c32c102b0139591a6
hawar <iaramkhzri@gmail.com>
2026-02-15 20:31:22 +0330

    rename editController to updateController

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,11 +1,11 @@
 import accountController from '@/controllers/account/account';
-import editController from '@/controllers/account/edit';
+import updateController from '@/controllers/account/update';
 import { authenticate, completed } from '@/middlewares/auth';
 import { Router } from 'express';
 
 const router = Router();
 
 router.get('/', authenticate, completed, accountController);
-router.put('/', authenticate, completed, editController);
+router.put('/', authenticate, completed, updateController);
 
 export default router;
